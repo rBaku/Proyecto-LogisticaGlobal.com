c3e71d6a5fad9a1ed642e31146f4f230773c86e5
Jorge Moreno <jorge.morenot@sansano.usm.cl>
2025-05-12 18:04:35 -0400

    conexion a db de azure

--- server/db.js
+++ server/db.js
@@ -2,25 +2,22 @@ const { Pool } = require('pg');
 const { DefaultAzureCredential } = require('@azure/identity');
 require('dotenv').config();
 
-async function getAzureToken() {
-  const credential = new DefaultAzureCredential();
-  const token = await credential.getToken('https://ossrdbms-aad.database.windows.net');
-  return token.token;
-}
-
 let pool;
+let mailUser='jorge.morenot@sansano.usm.cl'
 
 async function initializePool() {
-  const accessToken = await getAzureToken();
+
+  const credential = new DefaultAzureCredential();
+  const token = await credential.getToken("https://ossrdbms-aad.database.windows.net/.default");
   
   pool = new Pool({
-    host: process.env.DB_HOST, // Leído desde .env
-    user: process.env.DB_USER, // Leído desde .env
-    database: process.env.DB_NAME, // Leído desde .env
-    port: parseInt(process.env.DB_PORT, 10), // Leído desde .env y convertido a número
-    password: accessToken,
+    host: 'logisticabasedatos.postgres.database.azure.com',
+    user: mailUser,
+    database: 'postgres',
+    port: 5432,
+    password: token.token,
     ssl: {
-      rejectUnauthorized: false
+      rejectUnauthorized: false,
     },
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 5000
@@ -41,7 +38,7 @@ module.exports = {
       const result = await pool.query(text, params);
       return result;
     } catch (err) {
-      console.error('Error en la consulta:', err);
+      console.error(`Error en la consulta "${text}":`, err);
       throw err;
     }
   }
